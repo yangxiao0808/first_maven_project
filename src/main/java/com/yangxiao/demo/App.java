a02d5965ab1b4d91a6072e2978a473875840fbc9
杨霄 <1326373630@qq.com>
2019-06-17 15:40:16 +0800

    yangxiao

--- src/main/java/com/yangxiao/demo/App.java
+++ src/main/java/com/yangxiao/demo/App.java
@@ -7,18 +7,19 @@ package com.yangxiao.demo;
 public class App 
 {
     public static void main(String[] args ){
-        int a=-10;
-        int b=-20;
-        int c=b-a;
-        int d=add(a,b);
-        System.out.println(d);
+        int a = -10;
+        int b = -20;
+        int c = add(a,b);
+        System.out.println(c);
     }
 
     public static int add(int a,int b){
-        return abs(a)+abs(b);
+        return abs(a) + abs(b);
     }
 
     public static int abs(int a){
         return Math.abs(a);
     }
+
+
 }
